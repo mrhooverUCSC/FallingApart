4843d0593d5b729fc50b02bea5f67e0d02832cd0
matthooverslac <61717155+mrhooverUCSC@users.noreply.github.com>
2022-11-21 13:53:52 -0800

    Breaking It All
    going to try and rework WolfScript completely, safety commit

--- Assets/Scripts/WolfScript.cs
+++ Assets/Scripts/WolfScript.cs
@@ -9,9 +9,6 @@ public class WolfScript : MonoBehaviour
     public static readonly Vector3 South = new Vector3(0, 0, -1);
     public static readonly Vector3 East = new Vector3(1, 0, 0);
     public static readonly Vector3 West = new Vector3(-1, 0, 0);
-    [SerializeField] LayerMask junctionMask;
-    [SerializeField] LayerMask deadEndMask;
-    [SerializeField] LayerMask wallMask;
     enum direction
     {
         NORTH,
@@ -36,7 +33,7 @@ public class WolfScript : MonoBehaviour
         detectPlayer();
         if (running && Vector3.Distance(transform.position, targetPosition) > .2f)
         {
-            Debug.Log(transform.position + " | " + targetPosition);
+            //Debug.Log(transform.position + " | " + targetPosition);
             cController.SimpleMove((targetPosition - transform.position).normalized * 2);
         }
         else if(running)
@@ -51,76 +48,92 @@ public class WolfScript : MonoBehaviour
         {
             if (Mathf.Abs(gameObject.transform.position.x - player.gameObject.transform.position.x) < 1.25 && gameObject.transform.position.z < player.gameObject.transform.position.z)
             {
-                //Debug.Log("player in north");
+                Debug.Log("player in north");
                 chooseDirection(direction.NORTH);
             }
             else if (Mathf.Abs(gameObject.transform.position.x - player.gameObject.transform.position.x) < 1.25 && gameObject.transform.position.z > player.gameObject.transform.position.z)
             {
-                //Debug.Log("player in south");
+                Debug.Log("player in south");
                 chooseDirection(direction.SOUTH);
             }
             else if (Mathf.Abs(gameObject.transform.position.z - player.gameObject.transform.position.z) < 1.25 && gameObject.transform.position.x < player.gameObject.transform.position.x)
             {
-                //Debug.Log("player in east");
+                Debug.Log("player in east");
                 chooseDirection(direction.EAST);
             }
             else if (Mathf.Abs(gameObject.transform.position.z - player.gameObject.transform.position.z) < 1.25 && gameObject.transform.position.x > player.gameObject.transform.position.x)
             {
-                //Debug.Log("player in west");
+                Debug.Log("player in west");
                 chooseDirection(direction.WEST);
             }
         }
     }
     //raycast in directions player is not in. find the best one, then go there.
-    void chooseDirection(direction dir)
+    void chooseDirection(direction dir) //dir is direction the player is in
     {
+        Debug.Log("---- STARTING CHOOSEDIRECTION ----");
         RaycastHit hit;
-        Collider target = null;
-        //if nothing is in the way, look for a junction, and save it if there is one.
-        if (dir != direction.NORTH && !Physics.Raycast(transform.position, North, out hit, 25, wallMask))
-        {
-            Debug.DrawRay(transform.position, North * 25, Color.cyan, .5f, true);
-            if(Physics.Raycast(transform.position, North, out hit, 25, junctionMask))
+        //Layers are just a number, so looping is super easy. I'm using 'i' just for readability in the actual loop, as a hold over for "Layers 13-16"
+        LayerMask wallMask = LayerMask.NameToLayer("wall");
+        LayerMask temp = LayerMask.NameToLayer("junctionP1"); //iteratable layermask
+        List<KeyValuePair<Collider, direction>> junctions = new List<KeyValuePair<Collider, direction>>(); //a list to keep track of what the wolf finds, in order of prority
+        //for each prority, check if there is that prority in each direction the player is not coming from, then save it if there is
+        for (int i = 1; i < 4; ++i, ++temp){
+            Debug.Log("-- Starting Layer " + i + " | " + LayerMask.LayerToName(temp));
+            if (dir != direction.NORTH)// && !Physics.Raycast(transform.position, North, out hit, 25, wallMask))
             {
-                Debug.Log("junction to north");
-                target = hit.collider;
+                Debug.Log("checking north");
+                Debug.DrawRay(transform.position, North * 25, Color.cyan, .5f, true);
+                if (Physics.Raycast(transform.position, North, out hit, 25, ~temp))
+                {
+                    Debug.Log("found junction priority " + i + " to north");
+                    junctions.Add(new KeyValuePair<Collider, direction>(hit.collider, direction.NORTH));
+                }
+            }
+            if (dir != direction.SOUTH)// && !Physics.Raycast(transform.position, South, out hit, 25, wallMask))
+            {
+                Debug.Log("checking south");
+                Debug.DrawRay(transform.position, South * 25, Color.cyan, .5f, true);
+                if (Physics.Raycast(transform.position, South, out hit, 25, ~temp))
+                {
+                    Debug.Log("found junction priority " + i + " to south");
+                    junctions.Add(new KeyValuePair<Collider, direction>(hit.collider, direction.SOUTH));
+                }
+            }
+            if (dir != direction.EAST)// && !Physics.Raycast(transform.position, East, out hit, 25, wallMask))
+            {
+                Debug.Log("checking east");
+                Debug.DrawRay(transform.position, East * 25, Color.cyan, .5f, true);
+                if (Physics.Raycast(transform.position, East, out hit, 25, ~temp))
+                {
+                    Debug.Log("found junction priority " + i + " to east");
+                    junctions.Add(new KeyValuePair<Collider, direction>(hit.collider, direction.EAST));
+                }
+            }
+            if (dir != direction.WEST)// && !Physics.Raycast(transform.position, West, out hit, 25, wallMask))
+            {
+                Debug.Log("checking west");
+                Debug.DrawRay(transform.position, West * 25, Color.cyan, .5f, true);
+                if (Physics.Raycast(transform.position, West, out hit, 25, ~temp))
+                {
+                    Debug.Log("found junction priority " + i + " to west");
+                    junctions.Add(new KeyValuePair<Collider, direction>(hit.collider, direction.WEST));
+                }
             }
         }
-        if (dir != direction.SOUTH && !Physics.Raycast(transform.position, South, out hit, 25, wallMask))
+        //if you found a junction, go to the first one found
+        foreach(KeyValuePair<Collider, direction> i in junctions)
         {
-            Debug.DrawRay(transform.position, South * 25, Color.cyan, .5f, true);
-            if (Physics.Raycast(transform.position, South, out hit, 25, junctionMask))
-            {
-                Debug.Log("junction to south");
-                target = hit.collider;
-            }
+            Debug.Log(i);
         }
-        if (dir != direction.EAST && !Physics.Raycast(transform.position, East, out hit, 25, wallMask))
+        if (junctions.Count > 0)
         {
-            Debug.DrawRay(transform.position, East * 25, Color.cyan, .5f, true);
-            if (Physics.Raycast(transform.position, East, out hit, 25, junctionMask))
-            {
-                Debug.Log("junction to east");
-                target = hit.collider;
-            }
-        }
-        if (dir != direction.WEST && !Physics.Raycast(transform.position, West, out hit, 25, wallMask))
-        {
-            Debug.DrawRay(transform.position, West * 25, Color.cyan, .5f, true);
-            if (Physics.Raycast(transform.position, West, out hit, 25, junctionMask))
-            {
-                Debug.Log("junction to west");
-                target = hit.collider;
-            }
-        }
-        //if you found a junction, go to it
-        if (target != null)
-        {
-            Debug.Log(target.name);
+            Debug.Log(junctions[0]);
             running = true;
-            targetPosition = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+            targetPosition = new Vector3(junctions[0].Key.transform.position.x, transform.position.y, junctions[0].Key.transform.position.z);
             Debug.Log(targetPosition);
+            return;
         }
-        //otherwise, repeat for deadends instead of junctions
+        Debug.LogError("Failed to find any junctions for Wolf1 from player in direction " + dir);
     }
 }
